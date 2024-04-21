cc3a2a5045774beb163db3ed185270746c703cdb
thiomajid <82466066+ATOM36@users.noreply.github.com>
2024-04-21 03:06:30 +0100

    Fixed analytics deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { withIcons, withMdx, withSitemap } from "./src/utils/integrations";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   site: "https://thiomajid.vercel.app",
   integrations: [withMdx(), withSitemap(), withIcons(), tailwind()],
   markdown: {
@@ -21,4 +19,9 @@ export default defineConfig({
     },
   },
   experimental: {},
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
